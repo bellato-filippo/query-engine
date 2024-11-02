2ee525f7989a805ba6177cbf4c02109a676b7afe
bellato-filippo <bellatofilippo@gmail.com>
2024-11-02 16:55:12 +0100

    Aggiunto override di equals

--- src/main/scala/Models/Query.scala
+++ src/main/scala/Models/Query.scala
@@ -3,7 +3,11 @@ package Models
 enum TermType:
     case Var, Cons
 
-class Term(name: String, termType: TermType):
+class Term(val name: String, val termType: TermType):
+    override def equals(that: Any): Boolean = that match
+        case t: Term => this.name == t.name && this.termType == t.termType
+        case _ => false
+    
     override def toString(): String = 
         if (termType == TermType.Cons)
             s"\'$name\'"
@@ -13,12 +17,20 @@ class Term(name: String, termType: TermType):
 class Variable(name: String) extends Term(name, TermType.Var)
 class Constant(name: String, value: String) extends Term(name, TermType.Cons)
 
-class Atom(name: String, terms: List[Term]):
+class Atom(val name: String, val terms: List[Term]):
+    override def equals(that: Any): Boolean = that match
+        case a: Atom => this.name == a.name && this.terms == a.terms
+        case _ => false
+    
     override def toString(): String = 
         s"$name(${terms.map(_.toString).mkString(",")})"
 
 class Head(terms: List[Term]) extends Atom("Answer", terms)
 
-class Query(queryId: Int, head: Head, body: Set[Atom]):
+class Query(val queryId: Int, val head: Head, val body: Set[Atom]):
+    override def equals(that: Any): Boolean = that match
+        case q: Query => this.queryId == q.queryId && this.head == q.head && this.body == q.body
+        case _ => false
+    
     override def toString(): String = 
         s"${head.toString()} :- ${body.map(_.toString).mkString(",")}"
