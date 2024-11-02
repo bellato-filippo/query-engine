7c143cfd67bd5106b89e615d190576fbfa599044
bellato-filippo <bellatofilippo@gmail.com>
2024-11-02 16:35:15 +0100

    Implementato alcune unit test

--- src/test/scala/MySuite.scala
+++ src/test/scala/MySuite.scala
@@ -1,9 +1,15 @@
-// For more information on writing tests, see
-// https://scalameta.org/munit/docs/getting-started.html
+import Models.*
+
 class MySuite extends munit.FunSuite {
   test("example test that succeeds") {
     val obtained = 42
     val expected = 42
     assertEquals(obtained, expected)
+  }
+
+  test("Pretty printing") {
+      val cq: Q = Q(1, H(List(V("x"), V("y"), V("z"))), Set(A("R", List(V("x"), V("y"))), A("S", List(V("y"), V("z")))))
+      val cqString = "Answer(x,y,z) :- R(x,y),S(y,z)"
+      assertEquals(cq.toString(), cqString)
   }
 }
