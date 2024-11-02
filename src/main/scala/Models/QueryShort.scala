f8fe11401946ce2e4a51020e38163477aa89f12e
bellato-filippo <bellatofilippo@gmail.com>
2024-11-02 16:34:53 +0100

    Creato degli alias con le iniziali per velocizzare i test

---
+++ src/main/scala/Models/QueryShort.scala
@@ -0,0 +1,10 @@
+package Models
+
+class T(name: String, termType: TermType, value: String) extends Term(name, termType)
+
+class V(name: String) extends Variable(name)
+class C(name: String, value: String) extends Constant(name, value)
+
+class A(name: String, terms: List[Term]) extends Atom(name, terms)
+class H(terms: List[Term]) extends Head(terms)
+class Q(queryId: Int, head: Head, body: Set[Atom]) extends Query(queryId, head, body)
