96f441bf3ae77fcd65d3c0f7333f16f3b14a013c
bellato-filippo <bellatofilippo@gmail.com>
2024-11-02 16:34:32 +0100

    Rimosso l'attributo superfluo in Term

--- src/main/scala/Models/Query.scala
+++ src/main/scala/Models/Query.scala
@@ -3,22 +3,22 @@ package Models
 enum TermType:
     case Var, Cons
 
-class Term(name: String, termType: TermType, value: String):
+class Term(name: String, termType: TermType):
     override def toString(): String = 
         if (termType == TermType.Cons)
-            s"\'$value\'"
+            s"\'$name\'"
         else
             name
 
-class Variable(name: String) extends Term(name, TermType.Var, null)
-class Constant(name: String, value: String) extends Term(name, TermType.Cons, value)
+class Variable(name: String) extends Term(name, TermType.Var)
+class Constant(name: String, value: String) extends Term(name, TermType.Cons)
 
 class Atom(name: String, terms: List[Term]):
     override def toString(): String = 
         s"$name(${terms.map(_.toString).mkString(",")})"
 
 class Head(terms: List[Term]) extends Atom("Answer", terms)
-        
+
 class Query(queryId: Int, head: Head, body: Set[Atom]):
     override def toString(): String = 
         s"${head.toString()} :- ${body.map(_.toString).mkString(",")}"
