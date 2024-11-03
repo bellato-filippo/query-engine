ad3a368db9544c8b73e13e95fcef7090eeaff2ad
Pizzox <filippopizzo03@gmail.com>
2024-11-03 17:21:49 +0100

    class hypergraph created and method isEar done

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,6 +2,9 @@ import Models.*
 
 object Main extends App {
   val cq: Query = Query(1, Head(List(Variable("x"), Variable("y"), Variable("z"))), Set(Atom("R", List(Variable("x"), Variable("y"))), Atom("S", List(Variable("y"), Variable("z")))))
-  
-  println(cq.toString())
+  val cq2: Query = Query(2, Head(List()), Set(Atom("R", List(Variable("x"), Variable("y"))), Atom("R", List(Variable("y"), Variable("z"))), Atom("R", List(Variable("x"), Variable("z")))))
+  val hg: Hypergraph = Hypergraph(cq2)
+  println(cq2.toString())
+  println(hg.toString())
+  println(hg.isEar(Set(Variable("y"), Variable("x"))))
 }
