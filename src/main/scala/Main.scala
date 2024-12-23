4671e13670e777a8434d96a22567e5626ec80e32
bellato-filippo <bellatofilippo@gmail.com>
2024-12-23 17:13:50 +0100

    Aggiunti test con csv

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,11 +3,54 @@ import Services.*
 import Models.*
 
 
-object Main extends App {
-  val q1 = QueryParserService.parse("Answer(x2) :- R1(x1, x2, x3), R2(x2, 'a'), R3(x3, 'b', 'c')")
-  val q2 = QueryParserService.parse("Answer(y2) :- R2(y1, y4), R1(y1, y2, y3)")
-  q1.queryId = 411
-  q2.queryId = 412
-  ContainmentService.isContainedIn(q1, q2, true)
-  // assertEquals(ContainmentService.isContainedIn(q2, q1, true), false)
-}
+object Main extends App:
+  val q1 = QueryParserService.parse("Answer() :- A(x, y), B(y, z), C(z, w, u, 'V')")
+  q1.queryId = 1
+
+  val q2 = QueryParserService.parse("Answer() :- A(x, y), B(y, z), C(z, w, u, 'V'), D(w, y)")
+  q2.queryId = 2
+
+  val q3 = QueryParserService.parse("Answer() :- A(x, x), D(x, x)")
+  q3.queryId = 3
+
+  val q4 = QueryParserService.parse("Answer(x) :- E(x, x)")
+  q4.queryId = 4
+
+  val q5 = QueryParserService.parse("Answer(x) :- E(x, y), E(y, z)")
+  q5.queryId = 5
+
+  val q6 = QueryParserService.parse("Answer(w) :- E(w, w), E(w, u)")
+  q6.queryId = 6
+
+  val q7 = QueryParserService.parse("Answer(x) :- E(x, x), E(x, y), E(y, z)")
+  q7.queryId = 7
+
+  val q8 = QueryParserService.parse("Answer(y, z) :- E(y, z), E(z, z), E(z, w)")
+  q8.queryId = 8
+
+  val q9 = QueryParserService.parse("Answer(x, y) :- E(x, y), E(y, z), E(z, y), E(y, w)")
+  q9.queryId = 9
+
+  val q10 = QueryParserService.parse("Answer(z, z) :- E(z, y), E(y, z), E(y, w)")
+  q10.queryId = 10
+
+  val queries = List(q1, q2, q3, q4, q5, q6, q7, q8, q9, q10)
+
+  // main
+  val mainFile = new java.io.PrintWriter(s"./src/test/results/main-output.csv")
+  mainFile.println(s"queryId,isAcyclic,isMinimal")
+  for (query <- queries) {
+    mainFile.println(s"${query.queryId},${query.isAcyclic},${query.isMinimal}")
+  }
+
+  // containment
+  val containmentFile = new java.io.PrintWriter(s"./src/test/results/containment-output.csv")
+  containmentFile.println(s"queryId1,queryId2,isContainedIn")
+  for (query1 <- queries) {
+    for (query2 <- queries) {
+      containmentFile.println(s"${query1.queryId},${query2.queryId},${query1 isContainedIn query2}")
+    }
+  }
+
+  mainFile.close()
+  containmentFile.close()
