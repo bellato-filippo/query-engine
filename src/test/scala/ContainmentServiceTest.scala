129fda2eac958ba0ad0e0d76ead09686e8ee2569
bellato-filippo <bellatofilippo@gmail.com>
2024-12-23 15:43:24 +0100

    Attivato i log nei test

--- src/test/scala/ContainmentServiceTest.scala
+++ src/test/scala/ContainmentServiceTest.scala
@@ -33,54 +33,54 @@ class ContainmentServiceTest extends munit.FunSuite {
         val q2 = QueryParserService.parse("Answer() :- R1(x1), S1(x1, x3)")
         q1.queryId = 413
         q2.queryId = 414
-        assertEquals(ContainmentService.isContainedIn(q1, q2, false), false)
-        assertEquals(ContainmentService.isContainedIn(q2, q1, false), false)
+        assertEquals(ContainmentService.isContainedIn(q1, q2, true), false)
+        assertEquals(ContainmentService.isContainedIn(q2, q1, true), false)
 
     test("Lab4 E1 c"):
         val q1 = QueryParserService.parse("Answer(x1, x2, x3) :- S1(x1, x2), S2(x2, 'a'), S3(x3, 'b'), S4(x3, x1)")
         val q2 = QueryParserService.parse("Answer(y2, y3, y1) :- S4(y1, y2), S1(y2, y3), S2(y3, y4)")
         q1.queryId = 415
         q2.queryId = 416
-        assertEquals(ContainmentService.isContainedIn(q1, q2, false), true)
-        assertEquals(ContainmentService.isContainedIn(q2, q1, false), false)
+        assertEquals(ContainmentService.isContainedIn(q1, q2, true), true)
+        assertEquals(ContainmentService.isContainedIn(q2, q1, true), false)
 
     test("Lab4 E1 d"):
         val q1 = QueryParserService.parse("Answer(x1) :- R(x1, y1), S(y1, x2), R(x1, y2), S(y2, x2)")
         val q2 = QueryParserService.parse("Answer(u) :- R(u, v), S(v, w), R(x, v), R(x, y)")
         q1.queryId = 417
         q2.queryId = 418
-        assertEquals(ContainmentService.isContainedIn(q1, q2, false), true)
-        assertEquals(ContainmentService.isContainedIn(q2, q1, false), true)
+        assertEquals(ContainmentService.isContainedIn(q1, q2, true), true)
+        assertEquals(ContainmentService.isContainedIn(q2, q1, true), true)
 
     test("Lab4 E2 a"):
         val q1 = QueryParserService.parse("Answer(y1, y2, y3) :- R1(y1, y3), R6(y3, 'a'), R3(y2, y1), R5('b', y1), R2(y3, y2), R4(y2, 'a')")
         val q2 = QueryParserService.parse("Answer(z, y, x) :- R2(x, y), R3(y, z), R1(z, x)")
         q1.queryId = 421
         q2.queryId = 422
-        assertEquals(ContainmentService.isContainedIn(q1, q2, false), true)
-        assertEquals(ContainmentService.isContainedIn(q2, q1, false), false)
+        assertEquals(ContainmentService.isContainedIn(q1, q2, true), true)
+        assertEquals(ContainmentService.isContainedIn(q2, q1, true), false)
 
     test("Lab4 E2 b"):
         val q1 = QueryParserService.parse("Answer(x2, x3) :- R1(x1, x2, 'a'), R2(x2, x1, x3), R3(x3, 'c'), R4('d', x1)")
         val q2 = QueryParserService.parse("Answer(y2, y3) :- R2(y2, y1, 'e'), R1(y1, y2, y3)")
         q1.queryId = 423
         q2.queryId = 424
-        assertEquals(ContainmentService.isContainedIn(q1, q2, false), false)
-        assertEquals(ContainmentService.isContainedIn(q2, q1, false), false)
+        assertEquals(ContainmentService.isContainedIn(q1, q2, true), false)
+        assertEquals(ContainmentService.isContainedIn(q2, q1, true), false)
 
     test("Lab4 E2 c"):
         val q1 = QueryParserService.parse("Answer(y2, y3, y1) :- S4(y1, y2), S1(y2, y3), S2(y3, y4)")
         val q2 = QueryParserService.parse("Answer(x1, x2, x3) :- S1(x1, x2), S2(x2, 'a'), S3(x3, 'b'), S4(x3, x1)")
         q1.queryId = 425
         q2.queryId = 426
-        assertEquals(ContainmentService.isContainedIn(q1, q2, false), false)
-        assertEquals(ContainmentService.isContainedIn(q2, q1, false), true)
+        assertEquals(ContainmentService.isContainedIn(q1, q2, true), false)
+        assertEquals(ContainmentService.isContainedIn(q2, q1, true), true)
 
     test("Lab4 E2 d"):
         val q1 = QueryParserService.parse("Answer(x1) :- R(x1, y1), S(y1, x2), R(x1, y2), S(y2, x2)")
         val q2 = QueryParserService.parse("Answer(u) :- R(u, v), S(v, w), R(x, v), R(x, y)")
         q1.queryId = 427
         q2.queryId = 428
-        assertEquals(ContainmentService.isContainedIn(q1, q2, false), true)
-        assertEquals(ContainmentService.isContainedIn(q2, q1, false), true)
+        assertEquals(ContainmentService.isContainedIn(q1, q2, true), true)
+        assertEquals(ContainmentService.isContainedIn(q2, q1, true), true)
 }
--- src/test/scala/QueryMinimizerServiceTest.scala
+++ src/test/scala/QueryMinimizerServiceTest.scala
@@ -6,34 +6,34 @@ class QueryMinimizerServiceTest extends munit.FunSuite:
     test("Lab4 E3 a"):
         val q = QueryParserService.parse("Answer(x) :- R(x, y), S(z)")
         q.queryId = 431
-        assertEquals(QueryMinimizerService.isMinimal(q, false), true)
+        assertEquals(QueryMinimizerService.isMinimal(q, true), true)
         
     test("Lab4 E3 d"):
         val q = QueryParserService.parse("Answer(x, y, z) :- R(x, y, z), R(u, v, z), R(u, y, r), S(x, y), S(u, v)")
         q.queryId = 432
-        assertEquals(QueryMinimizerService.isMinimal(q, false), false)
+        assertEquals(QueryMinimizerService.isMinimal(q, true), false)
 
     test("Lab4 E3 e"):
         val q = QueryParserService.parse("Answer(y) :- R1(x, y), R1(v, u), R1(v, y), R2(v, x, u, y), R2(u, y, v, x)")
         q.queryId = 433
-        assertEquals(QueryMinimizerService.isMinimal(q, false), true)
+        assertEquals(QueryMinimizerService.isMinimal(q, true), true)
 
     test("Lab4 E3 f"):
         val q = QueryParserService.parse("Answer(x) :- R(x, y), S(z)")
         q.queryId = 434
-        assertEquals(QueryMinimizerService.isMinimal(q, false), true)
+        assertEquals(QueryMinimizerService.isMinimal(q, true), true)
 
     test("Lab4 E3 g"):
         val q = QueryParserService.parse("Answer(y, z) :- R(x, y), R(y, z), R(z, x), R(x, u), S(u, y), S(y, u)")
         q.queryId = 435
-        assertEquals(QueryMinimizerService.isMinimal(q, false), true)
+        assertEquals(QueryMinimizerService.isMinimal(q, true), true)
 
     test("Lab4 E3 h"):
         val q = QueryParserService.parse("Answer(x, y) :- R(x, z), R(w, y)")
         q.queryId = 436
-        assertEquals(QueryMinimizerService.isMinimal(q, false), true)
+        assertEquals(QueryMinimizerService.isMinimal(q, true), true)
 
     test("Lab4 E3 i"):
         val q = QueryParserService.parse("Answer(z, x, u) :- R(x, y, u), S(y, u, x), R(u, y, x), S(z, x, y), R(s, y, x)")
         q.queryId = 437
-        assertEquals(QueryMinimizerService.isMinimal(q, false), false)
+        assertEquals(QueryMinimizerService.isMinimal(q, true), false)
